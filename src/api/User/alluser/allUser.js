26bc5bb7176f9996e607eb0fe97a15b2b5f2edc8
leekyungho112 <epsoe@naver.com>
2020-08-07 16:12:16 +0900

    업로드 추가

---
+++ src/api/User/alluser/allUser.js
@@ -0,0 +1,17 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+
+export default {
+    Query: {
+        allUser: async(_, __, {request, isAuthenticated }) => {
+            isAuthenticated(request);
+            const { user } = request;
+            return prisma.users({
+                where: {
+                    id_not_in: [user.id]
+                },
+                orderBy: "createdAt_DESC"
+            });
+        }
+    }
+};
